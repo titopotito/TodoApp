6c318a71671eda6d56faaee373736135daf9012d
titopotito <jdf.tito@gmail.com>
2022-09-22 20:26:47 +0800

    Added and connected to database.

--- config/static/js/script.js
+++ config/static/js/script.js
@@ -1,27 +1,44 @@
 const todoList = document.getElementById("todo-list");
 const todoForm = document.getElementById("todo-form");
 const todoInput = document.getElementById("todo-input");
+const csrftokenHtml = document.querySelector("form > input");
 
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const todo = new TodoHtml(todoInput.value);
-    todo.addToList(todoList);
-    todoInput.value = "";
+    const text = todoInput.value;
+    const csrftoken = csrftokenHtml.value;
+
+    fetch("addTodo", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-CSRFToken": csrftoken,
+        },
+        body: JSON.stringify({ text }),
+    })
+        .then((response) => response.json())
+        .then((result) => {
+            const todo = new TodoHtml(result);
+            todo.addToList(todoList);
+            todoInput.value = "";
+        })
+        .catch((error) => console.log(error));
 });
 
 class TodoHtml {
     constructor(todo) {
-        this.todoHtml = document.createElement("div");
+        this.todoHtml = document.createElement("form");
         this.textContainer = document.createElement("span");
         this.completeBtnContainer = document.createElement("span");
         this.deleteBtnContainer = document.createElement("span");
         this.completeBtn = document.createElement("button");
         this.deleteBtn = document.createElement("button");
+        this.idContainer = document.createElement("input");
         this.createTodoHtml(todo);
     }
 
     createTodoHtml(todo) {
-        this.textContainer.innerText = todo;
+        this.textContainer.innerText = todo.text;
         this.textContainer.classList = ["col-10"];
         this.completeBtn.innerText = "O";
         this.deleteBtn.innerText = "X";
@@ -29,9 +46,12 @@ class TodoHtml {
         this.completeBtnContainer.classList = ["col-1"];
         this.deleteBtnContainer.append(this.deleteBtn);
         this.deleteBtnContainer.classList = ["col-1"];
+        this.idContainer.type = "hidden";
+        this.idContainer.value = todo.id;
         this.todoHtml.append(this.textContainer);
         this.todoHtml.append(this.completeBtnContainer);
         this.todoHtml.append(this.deleteBtnContainer);
+        this.todoHtml.append(this.idContainer);
         this.todoHtml.classList = ["row"];
     }
 
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+
+from .models import Todo
+import json
 
 def index(request):
-    return render(request, 'todo.html')
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos
+    }
+    return render(request, 'todo.html', {'todos': todos})
+
+def addTodo(request):
+    if request.method == 'POST':
+        data = request.body
+        json_object = json.loads(data)
+        text = json_object['text']
+        todo = Todo(text=text)
+        todo.save()
+        todo_id = todo.pk
+
+    return JsonResponse({'id': todo_id, 'text': text}, safe=False)
